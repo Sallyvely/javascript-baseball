a857c2908f8360c6b4297fc333cc97dcfdb2a713
Sallyvely <tpdus4159@naver.com>
2022-11-25 15:32:16 +0900

    refactor: 코드 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,166 +1,101 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const MESSAGE = require('./Message');
-const ERROR_MESSAGE = require('./Message');
-const GAME = require('./Message');
+const OutputView = require('./OutputView');
+const InputView = require('./InputView');
 class App {
   play() {    
     let computerRandomNum = this.createRandomNumber();
-    let userInputNum = this.InputUserNumber();
-
-    if(this.checkUserNumVaildation(userInputNum)){
-      MissionUtils.Console.print(this.calResult(computerRandomNum, userInputNum));
-    }
+    let userInputNum = InputView.InputUserNumber();
+    MissionUtils.Console.print(this.calResult(computerRandomNum, userInputNum));
     
-    while(!this.checkThreeStrike(computerRandomNum, userInputNum) && this.checkUserNumVaildation(userInputNum)){     
-      userInputNum = this.InputUserNumber();  
-    }}
+    while(!this.checkThreeStrike(computerRandomNum, userInputNum)){     
+      userInputNum = InputView.InputUserNumber();
+      MissionUtils.Console.print(this.calResult(computerRandomNum, userInputNum));  
+    }
+  }
+
+  gameStart() {
+
+  } 
 
   createRandomNumber() {
     let computerNumber = "";
     let count = 0;
     while(count < MESSAGE.number.gameNumLength){
-      let charNum = MissionUtils.Random.pickNumberInRange(1, 9);
+      const charNum = MissionUtils.Random.pickNumberInRange(1, 9);
       if(!computerNumber.includes(String(charNum))){
         computerNumber += String(charNum);
         count++;
       }}
-      return computerNumber;
-    }
+    return computerNumber;
+  }
 
-    InputUserNumber(){
-      let userInput = 0;
-      MissionUtils.Console.readLine(MESSAGE.gameMessage.Input, (userNum) => {
-        userInput = userNum;
-      })
-        return userInput;
-    }
-
-    strike(computerRandomNum, userInputNum){
-      let strikeScore = 0;
-      for(let index = 0; index < MESSAGE.number.gameNumLength; index++){
-        if(String(computerRandomNum).charAt(index) === String(userInputNum).charAt(index)){
-          strikeScore += 1;  
-        }
-      }  
-      return strikeScore;
-    }
+  strike(computerRandomNum, userInputNum){
+    let strikeScore = 0;
+    for(let index = 0; index < MESSAGE.number.gameNumLength; index++){
+      if(String(computerRandomNum).charAt(index) === String(userInputNum).charAt(index)){
+        strikeScore += 1;  
+      }
+    }  
+    return strikeScore;
+  }
 
     /*볼인지 점수 계산하는 기능*/
-    ball(computerRandomNum, userInputNum){
-      let ballScore = 0;
-      for(let index = 0; index < MESSAGE.number.gameNumLength; index++){
-        if((computerRandomNum).includes(String(userInputNum).charAt(index)) && !(String(computerRandomNum).charAt(index) === String(userInputNum).charAt(index))){
-          ballScore += 1;
-        }
+  ball(computerRandomNum, userInputNum){
+    let ballScore = 0;
+    for(let index = 0; index < MESSAGE.number.gameNumLength; index++){
+      if((computerRandomNum).includes(String(userInputNum).charAt(index)) && !(String(computerRandomNum).charAt(index) === String(userInputNum).charAt(index))){
+        ballScore += 1;
       }
-      return ballScore;
     }
-        /*볼과 스트라이크 점수 계산하는 기능*/
-  calResult(computerRandomNum, userInputNum){
-    let answer = "";
-    const score=this.saveScore(computerRandomNum, userInputNum);
-    if(score[0] == 0 && score[1] == 0){
-      answer = MESSAGE.score.nothing;
-    }
-    if(score[0] == 0 && score[1] >0){
-      answer = score[1] + MESSAGE.score.ball;
-    }
-    if(score[0] > 0 && score[1] == 0 && !score[0] == MESSAGE.number.gameNumLength){
-      answer = score[0] + MESSAGE.score.strike;
-    }
-    if(score[0] > 0 && score[1] > 0){
-      answer = score[1] + MESSAGE.score.ball + MESSAGE.score.space + score[0] + MESSAGE.score.strike;
-    }
-    return answer;
+    return ballScore;
   }
+  
+  /*볼과 스트라이크 점수 계산하는 기능*/
+  calResult(computerRandomNum, userInputNum){
+    const score=this.saveScore(computerRandomNum, userInputNum);
+    if(score[0] == 0 && score[1] == 0) return MESSAGE.score.nothing;
+  
+    if(score[0] == 0 && score[1] >0) return score[1] + MESSAGE.score.ball;
+    
+    if(score[0] > 0 && score[1] == 0) return score[0] + MESSAGE.score.strike;
+    
+    if(score[0] > 0 && score[1] > 0) return score[1] + MESSAGE.score.ball + MESSAGE.score.space + score[0] + MESSAGE.score.strike;
+    
+  }
+  
   saveScore(computerRandomNum, userInputNum) {
     let score = [];
     score.push(this.strike(computerRandomNum, userInputNum));
     score.push(this.ball(computerRandomNum, userInputNum));
     return score;
   }
-    /*3스트라이크 인지 확인하는 기능*/
+
   checkThreeStrike(computerRandomNum, userInputNum){
     if(this.strike(computerRandomNum, userInputNum) == MESSAGE.number.gameNumLength){
-      this.printThreeStrike();
-      this.selectGameEnd(userInputNum);
-      return true;
-      }
-      return false;
-    }
-
-  printThreeStrike() {
-    MissionUtils.Console.print(MESSAGE.score.threeStrike);
-    MissionUtils.Console.print(MESSAGE.gameMessage.successEnd);
-  }
-
-  /*유저가 입력한 수의 길이가 유효한지 확인하는 기능*/
-  checkUserNumLength(userInputNum){
-    if(userInputNum === '1' || userInputNum === '2'){
-      return true;
-    }
-    if(userInputNum.length == MESSAGE.number.gameNumLength){
+      OutputView.printThreeStrike();
+      this.selectGameEnd();
       return true;
     }
     return false;
-  }
-
-  /*유저가 입력한 수가 숫자가 맞는지 확인하는 기능*/
-  checkUserNumIsNum(userInputNum){
-    if(userInputNum === '1' || userInputNum === '2'){
-      return true;
-    }
-    if(userInputNum > 99 && userInputNum < 1000){
-      return true;
-    }
-    return false;
-  }
-  
-  /*유저가 입력한 수가 서로 다른 수 인지 확인하는 기능*/
-  checkUserNumIsDifferent(userInputNum){
-    if(userInputNum == MESSAGE.number.restartNumber || userInputNum == MESSAGE.number.endNumber){
-      return true;
-    }
-    if(userInputNum.charAt(0) == (userInputNum.charAt(1))){
-      return false;
-    }
-    if(userInputNum.charAt(0) == (userInputNum.charAt(2))){
-      return false;
-    }
-    if(userInputNum.charAt(1) == (userInputNum.charAt(2))){
-      return false;
-    }
-    return true;
-  }
-
-  /*입력 수가 유효한지 확인하는 기능*/
-  checkUserNumVaildation(userInputNum){
-    if(!this.checkUserNumLength(userInputNum)){
-      throw MESSAGE.gameMessage.INPUT_ERROR;
-    }
-    if(!this.checkUserNumIsNum(userInputNum)){
-      throw MESSAGE.gameMessage.INPUT_ERROR;
-    }
-    if(!this.checkUserNumIsDifferent(userInputNum)){
-      throw MESSAGE.gameMessage.INPUT_ERROR;
-    }
-    return true;
   }
 
   /*게임 종료 여부 선택하는 기능*/
-  selectGameEnd(userInputNum){
-    userInputNum = this.InputUserNumber();
-    if(userInputNum == MESSAGE.number.restartNumber){
-      MissionUtils.Console.print(MESSAGE.gameMessage.gameRestart);
-      this.play();
-      }
-    else if(userInputNum == MESSAGE.number.endNumber){
-      MissionUtils.Console.print(MESSAGE.gameMessage.gameEnd);
-      MissionUtils.Console.close();
+  selectGameEnd(){
+    if(InputView.InputRestartNumber() == MESSAGE.number.restartNumber){
+      this.gameRestart();
     }
-    else{
-      throw MESSAGE.gameMessage.INPUT_ERROR;
-    }
+    return this.gameEnd();
+  }
+
+  gameRestart() {
+    OutputView.printRestart();
+    this.play();
+  }
+  
+  gameEnd() {
+    OutputView.printEnd();
+    MissionUtils.Console.close();
   }
 }
 module.exports = App;
